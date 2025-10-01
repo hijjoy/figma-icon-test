bb70874edd2eaf461441438a466662687bea6b92
hyewon <hijjoy04@gmail.com>
2025-10-01 14:55:33 +0900

    feat: test

---
+++ packages/assets/icons/src/icons.ts
@@ -0,0 +1,11 @@
+import IcArrowRight from "../svg/ic-arrow-right.svg?react";
+
+// Auto-generated icon registry
+
+
+export const iconRegistry = {
+  IcArrowRight: {
+    component: IcArrowRight,
+    tags: ["ic", "arrow", "right"],
+  }
+};
